3e26013bd0f3ed470d9892f133ecc0dc1ce73b27
Kajal-Srivastav <kajal-5>
2025-11-29 12:11:39 +0530

    static folder public

--- app/page.js
+++ app/page.js
@@ -1,8 +1,18 @@
+import Image from "next/image";
+
+
 export default function Home() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Welcome to Dummy Products Store</h1>
       <p>Go to Products to explore all items.</p>
+
+      <Image
+        src="/Images/image.png"
+        width={400}
+        height={300}
+        alt="Store Banner"
+      />
     </div>
   );
 }
