f11d17fdd4f7fb09e8d85d0dfc3492ef160ff01e
Kajal-Srivastav <kajal-5>
2025-12-05 00:43:06 +0530

    fix-err

--- app/loginpage/page.js
+++ app/loginpage/page.js
@@ -111,7 +111,7 @@
 "use client";
 import LoginForm from "./loginform";
 
-export const dynamic = "force-dynamic"; // Prevent static prerendering
+// export const dynamic = "force-dynamic"; 
 
 import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
